7bac61f249fc113c853a04146d46680a928bd71f
Chunyang Xu <mail@xuchunyang.me>
2016-10-29 04:05:36 +0800

    Update link to melpa
    The old one redirects to the new one

--- melpa-dl-counts.rb
+++ melpa-dl-counts.rb
@@ -2,6 +2,6 @@
 require 'json'
 require 'open-uri'
 
-JSON.parse(open("http://melpa.milkbox.net/download_counts.json",&:read)).sort_by{|p,c| c}.reverse.each {|pkg, dl|
+JSON.parse(open("http://melpa.org/download_counts.json",&:read)).sort_by{|p,c| c}.reverse.each {|pkg, dl|
   printf "# %6d %-40s (describe-package '%s)\n", dl, pkg, pkg
 }
