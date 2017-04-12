8a7e7614448af4a8464f099473071fac7a7b4f4a
rubikitch <rubikitch@ruby-lang.org>
2017-04-13 08:17:03 +0900

    misc

--- inc-juku.php
+++ inc-juku.php
@@ -2,7 +2,7 @@
 <a href="http://www.mag2.com/m/0001373131.html">まぐまぐ殿堂入りメルマガ＋無制限メール相談＋elisp/文書添削</a>をやっています。<br />
      <ul>
      <li>Emacsユーザ用<strong>100%目標達成スキル</strong></li>
-     <li>何事にも屈しない<strong>鉄璧のマインド</strong></li>
+     <li>何事にも屈しない<strong>鉄壁のマインド</strong></li>
      <li><strong>理想の自分になる方法</strong></li>
      </ul>
      など豪華特典満載！<br />
--- inc-package-install.php
+++ inc-package-install.php
@@ -1,7 +1,7 @@
 <div id="outline-container-sec-2" class="outline-2">
 <h2 id="sec-2">インストール</h2>
 <div class="outline-text-2" id="text-2">
-<p> パッケージシステムを初めて使う人は以下の設定を ~/.emacs.d/init.el の先頭に加えてください。<br  /> </p>
+<p> パッケージシステムを初めて使う人は<br />以下の設定を ~/.emacs.d/init.el の<br />先頭に加えてください。</p>
 <div class="org-src-container">
 <pre class="src src-emacs-lisp">(package-initialize)
 (setq package-archives
@@ -10,11 +10,11 @@
         (<span style="font-style: italic;">"org"</span> . <span style="font-style: italic;">"http://orgmode.org/elpa/"</span>)))
 </pre>
 </div>
-<p> 初めて<?=$attrs['name'] ?>を使う方は以下のコマンドを実行します。<br  /> </p>
+<p> 初めて<?=$attrs['name'] ?>を使う方は<br />以下のコマンドを実行します。</p>
 <pre class="example">
 M-x package-install <?=$attrs['name'] ?>
 </pre>
-<p> アップグレードする方は、以下のコマンドでアップグレードしてください。<br  /> そのためにはpackage-utilsパッケージが必要です。<br  /> </p>
+<p> アップグレードする方は、<br />以下のコマンドでアップグレードしてください。<br  /> そのためにはpackage-utilsパッケージが必要です。<br  /> </p>
 <pre class="example">
 M-x package-install package-utils (初めてアップグレードする場合のみ)
 M-x package-utils-upgrade-by-name <?=$attrs['name'] ?>
